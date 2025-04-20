3bac1592e5837659656f04a6c413f81bfb470763
sujay-s-n <sn.s@northeastern.edu>
2025-04-20 01:11:29 -0400

    simplify cors implementation

--- backend/server.js
+++ backend/server.js
@@ -32,23 +32,30 @@ const allowedOrigins = [
 	"https://stockwise-demo.sleepysoul.cc",
 ];
 
-const corsOptions = {
-	origin: function (origin, callback) {
-		if (!origin || allowedOrigins.includes(origin)) {
-			callback(null, true);
-		} else {
-			console.warn(`CORS blocked for origin: ${origin}`);
-			callback(new Error("Not allowed by CORS"));
-		}
-	},
-	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-	allowedHeaders: ["Content-Type", "Authorization"],
-	credentials: true,
-	optionsSuccessStatus: 204,
-};
+// const corsOptions = {
+// 	origin: function (origin, callback) {
+// 		if (!origin || allowedOrigins.includes(origin)) {
+// 			callback(null, true);
+// 		} else {
+// 			console.warn(`CORS blocked for origin: ${origin}`);
+// 			callback(new Error("Not allowed by CORS"));
+// 		}
+// 	},
+// 	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+// 	allowedHeaders: ["Content-Type", "Authorization"],
+// 	credentials: true,
+// 	optionsSuccessStatus: 204,
+// };
 
+const corsOptions = {
+	origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://stockwise-demo.sleepysoul.cc"], // Allow this specific origin
+	methods: ["GET", "POST", "PUT", "DELETE"],
+	optionsSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // Handle preflight requests
+
+// app.use(cors(corsOptions));
+// app.options("*", cors(corsOptions)); // Handle preflight requests
 
 // --- Middleware ---
 app.use(express.json());
