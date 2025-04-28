c33fb8b3cb4755bba3f07c379883d7d3f956b4b8
Aniruddha Chitte <chitte.a@northeastern.edu>
2025-04-28 09:20:35 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -51,12 +51,13 @@ const allowedOrigins = [
 const corsOptions = {
 	origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://stockwise-demo.sleepysoul.cc"], // Allow this specific origin
 	methods: ["GET", "POST", "PUT", "DELETE"],
-	optionsSuccessStatus: 200,
+	credentials: true,
+	optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
 
 // app.use(cors(corsOptions));
-// app.options("*", cors(corsOptions)); // Handle preflight requests
+app.options("*", cors(corsOptions)); // Handle preflight requests
 
 // --- Middleware ---
 app.use(express.json());
